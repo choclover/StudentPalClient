ae72596534f344963a43787032f55b5ef7b5c9a0
Simon He <hemi12@tom.com>
2011-12-10 15:19:00 +0800

    2011-12-10 commit

--- src/com/studentpal/engine/request/RefreshAppListRequest.java
+++ src/com/studentpal/engine/request/RefreshAppListRequest.java
@@ -22,6 +22,10 @@ public class RefreshAppListRequest extends Request {
     this.targetPhoneNo = targetPhoneNum;
   }
 
+  //called by Phone client
+  public RefreshAppListRequest() {
+  }
+
   public String getName() {
     return Event.TASKNAME_RefreshAppList;
   }
@@ -47,7 +51,7 @@ public class RefreshAppListRequest extends Request {
           for (ClientAppInfo appInfo : appList) {
             JSONObject app = new JSONObject();
             app.put(Event.TAGNAME_APP_NAME, appInfo.getAppName());
-            //app.put(Event.TAGNAME_APP_CLASSNAME, appInfo.getAppClassname());
+            app.put(Event.TAGNAME_APP_CLASSNAME, appInfo.getAppClassname());
             app.put(Event.TAGNAME_APP_PKGNAME, appInfo.getAppPkgname());
             app.put(Event.TAGNAME_ACCESS_CATEGORY, 1);  //FIXME
 
